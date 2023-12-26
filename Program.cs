1fae42cc82d425db6ddf3f3c0d5cc284fc43e7de
Fylipe Araújo <fylipebabc@gmail.com>
2023-12-26 15:09:26 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,26 @@
-﻿using DesafioPOO.Models;
+using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        // Testando a classe Nokia
+        Console.WriteLine("Testando a classe Nokia:");
+        Nokia nokia = new Nokia("123456789", "Nokia ModeloX", "NOK123", 32);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        // Testando a classe Iphone
+        Console.WriteLine("Testando a classe Iphone:");
+        Iphone iphone = new Iphone("987654321", "iPhone Modelo12", "IPHONE456", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+
+        // Aguarda a entrada do usuário antes de encerrar o programa
+        Console.ReadLine();
+    }
+}
